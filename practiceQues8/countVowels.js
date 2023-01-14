1f4745065907a5411231f343232b4969211f1534
Sukhacoder02 <sukhmansindhipb05@gmail.com>
2023-01-15 00:45:54 +0530

    feat(string): implement countVowels

---
+++ practiceQues8/countVowels.js
@@ -0,0 +1,11 @@
+
+function isVowel(char) {
+    return char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u';
+}
+const countVowels = (string) => {
+    let count = 0;
+    [...string].forEach((char) => count += isVowel(char) ? 1 : 0);
+    return count;
+}
+
+console.log(countVowels('abcdefghijklmnopqrstuvxyz'));
