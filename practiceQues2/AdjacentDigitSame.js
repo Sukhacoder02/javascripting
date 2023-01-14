b46d096e1c6552d0423ed5e25c67ec41ea763fc5
Sukhacoder02 <sukhmansindhipb05@gmail.com>
2023-01-14 18:35:49 +0530

    feat(math): implement all digits same

---
+++ practiceQues2/AdjacentDigitSame.js
@@ -0,0 +1,25 @@
+
+const isAdjacentDigitSame = (number) => {
+    let lastDigit = number%10;
+    while(number != 0) {
+        let currDigit = number%10;
+        if(currDigit !== lastDigit) {
+            return false;
+        }
+        number = Math.floor(number/10);
+    }
+    return true;
+}
+const asciiZero = 48;
+function isAdjacentDigitSameVersion2(number) {
+    let numString = number.toString();
+    let firstDigit = numString.charCodeAt(0) - asciiZero;
+    for(let index=1;index<numString.length;index++) {
+        let currDigit = numString.charCodeAt(index) - asciiZero;
+        if(currDigit != firstDigit) {
+            return false;
+        }
+    }
+    return true;
+}
+
