f48fa76fb2691fd0361877e6df670338f110d59f
Sukhacoder02 <sukhmansindhipb05@gmail.com>
2023-01-15 22:40:16 +0530

    test: add testcases for exerciseJan13.js

--- exercise.test.js
+++ exercise.test.js
@@ -1,10 +1,11 @@
 
 const {checkArmstrong ,checkPrime } =  require('./exercise.js');
 
-test('is  371 armstrong?',() => {
+
+test(`is  371 armstrong?`,() => {
     expect(checkArmstrong(371)).toBeTruthy();
 });
 
-test('is 7 prime?', () => {
-    expect(checkPrime(7)).toBeTruthy();
+test(`is 17 prime?` , () => {
+    expect(checkPrime(17)).toBeTruthy();
 });
