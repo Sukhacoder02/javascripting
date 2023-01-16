272a176f90922162beea2fb205d1a43ff5ce464c
Sukhacoder02 <sukhmansindhipb05@gmail.com>
2023-01-16 23:24:32 +0530

    test: add tests for arrayUtilities

--- arrayUtilities/arrayUtilities.js
+++ arrayUtilities/arrayUtilities.js
@@ -1,9 +1,21 @@
 
 const doubleNumbers = (numbers) => {
+  if(!Array.isArray(numbers)) {
+    throw new Error('You must provide an array');
+  } 
+  return numbers.map((x)=>x*2);
+}; 
+
+
+const filterAllEvenNumbers = (numbers) => {
     if(!Array.isArray(numbers)) {
         throw new Error('You must provide an array');
     } 
-    return numbers.map((x)=>x*2);
-} 
+    if(numbers.some((number) => typeof(number) === 'string')) {
+        throw new Error('Input must be an array of integers');
+    }
+    return numbers.map((x)=>x*3).filter((x) => x%2==0);
+}
 
-module.exports = {doubleNumbers};
+// console.log(filterAllEvenNumbers(['a',2,3,4]));
+module.exports = {doubleNumbers,filterAllEvenNumbers};
--- arrayUtilities/arrayUtilities.test.js
+++ arrayUtilities/arrayUtilities.test.js
@@ -1,13 +1,30 @@
 
-const {doubleNumbers} = require('./arrayUtilities');
+const {doubleNumbers,filterAllEvenNumbers} = require('./arrayUtilities');
 
 describe('Array Utilities',() => {
-    describe('doubleNumbers', () => {
-        it('Should double the numbers when argument is an array',() => {
-            expect(doubleNumbers([1,2,3])).toEqual([2,4,6]);
-        });
-        it('Should throw an error when argument is not an array',() => {
-            expect(() => {doubleNumbers('abc')}).toThrow('You must provide an array');
-        }); 
+
+  describe('doubleNumbers', () => {
+    it('Should double the numbers when argument is an array',() => {
+      expect(doubleNumbers([1,2,3])).toEqual([2,4,6]);
     });
+
+    it('Should throw an error when argument is not an array',() => {
+      expect(() => {doubleNumbers('abc');}).toThrow('You must provide an array');
+    }); 
+  });
+
+
+  describe('filterAllEvenNumbers', () => {
+    it('Should first triple then filter all the even numnbers when input is an array',() => {
+        expect(filterAllEvenNumbers([1,2,3,4,5])).toEqual([6,12]);
+    });
+
+    it('Should throw an error when argument is not an array',() => {
+        expect(()=>{filterAllEvenNumbers('')}).toThrow('You must provide an array');
+    });
+
+    it('Should throw an error when array contains atleast one string',() => {
+        expect(()=>{filterAllEvenNumbers(['a',2,4])}).toThrow('Input must be an array of integers');
+    });
+  });
 });
